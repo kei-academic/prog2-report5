75893fca87b1df78885642de7bbfa8dea3cb03e0
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-09 09:01:26 +0900

    :sparkles: `Step2`
    Main.javaに例外処理を追加

--- app/src/main/java/jp/ac/uryukyu/ie/e215730/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215730/Main.java
@@ -1,8 +1,13 @@
 package jp.ac.uryukyu.ie.e215730;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NullPointerException {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Error: NullPointerException");
+            System.out.println(e.getMessage());
+        }
     }
 }
