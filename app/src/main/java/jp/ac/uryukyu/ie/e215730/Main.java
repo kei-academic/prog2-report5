2e90d6301f73b482049137c41120136fde41eaa2
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-13 14:40:03 +0900

    :+1: `Step3`
    Main.javaの修正

--- app/src/main/java/jp/ac/uryukyu/ie/e215730/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215730/Main.java
@@ -1,13 +1,8 @@
 package jp.ac.uryukyu.ie.e215730;
 
 public class Main {
-    public static void main(String[] args) throws NullPointerException {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("Error: NullPointerException");
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) throws NumberFormatException {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
